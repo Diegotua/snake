f20a359b740dcf874228997b60e68f49ba3abd1a
Diego Jose Tua Guevara <tuagdiego@gmail.com>
2022-12-05 18:04:10 -0400

    add apple

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ import pygame
 from pygame.locals import *
 import constants
 import time
+import random
 
-class snake():
+class apple():
     def __init__(self,parent_window):
         self.parent_window=parent_window
-        self.snake_body=pygame.image.load("image\circle.png")
-        self.x=600
-        self.y=300
-        self.direction="right"
+        self.image=pygame.image.load("apple.png")
+        self.x=120
+        self.y=120
     def draw(self):
-        self.parent_window.fill(constants.BG_COLOR)    
-        self.parent_window.blit(self.snake_body,(self.x,self.y))
+        self.parent_window.blit(self.image,(self.x,self.y))
+        pygame.display.flip()
+
+
+
+class snake():
+    def __init__(self,parent_window,lenght):
+        self.parent_window=parent_window
+        self.snake_body=pygame.image.load("image\circle.png")
+        self.direction="right"
+        self.lenght=lenght
+        self.x=[24]*lenght
+        self.y=[24]*lenght
+    def draw(self):
+        self.parent_window.fill(constants.BG_COLOR) 
+        for i in range(self.lenght):   
+            self.parent_window.blit(self.snake_body,(self.x[i],self.y[i]))
         pygame.display.flip()
 
     def move_left(self):
@@ -29,14 +44,18 @@ class snake():
            
        
     def walk(self):
+        for i in range(self.lenght-1,0,-1):
+            self.x[i]=self.x[i-1]
+            self.y[i]=self.y[i-1]
+
         if self.direction=="left":
-            self.x-=10
+            self.x[0]-=constants.SNAKE_BODY
         if self.direction=="right":
-            self.x+=10    
+            self.x[0]+=constants.SNAKE_BODY    
         if self.direction=="up":
-            self.y-=10
+            self.y[0]-=constants.SNAKE_BODY
         if self.direction=="down":
-            self.y+=10    
+            self.y[0]+=constants.SNAKE_BODY
         self.draw()
 class Game():
 
@@ -47,8 +66,10 @@ class Game():
         icon=pygame.image.load("image\snakes.png")
         pygame.display.set_icon(icon)
         self.window.fill(constants.BG_COLOR)
-        self.snake=snake(self.window)
+        self.snake=snake(self.window,4)
         self.snake.draw()
+        self.apple=apple(self.window,)
+        self.apple.draw()
 
         pygame.display.update()
 
@@ -58,7 +79,7 @@ class Game():
             for event in pygame.event.get():
                 if event.type==KEYDOWN:
                     if event.key==K_ESCAPE:
-                        running=False
+                        running=False                                                
                     if event.key ==K_LEFT:
                         self.snake.move_left()
 
@@ -74,6 +95,7 @@ class Game():
                     running=False 
 
             self.snake.walk()
+            self.apple.draw()
             time.sleep(0.1)               
 game=Game()
 game.run()
